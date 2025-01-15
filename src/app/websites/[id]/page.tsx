360c322a9c65c18afb242b43602b288b1577895e
Ben Perram <ben.perram@lumi.com.au>
2025-01-16 05:51:18 +1100

    fix build errors

--- src/app/websites/[id]/page.tsx
+++ src/app/websites/[id]/page.tsx
@@ -1,5 +1,13 @@
 import WebsitePageClient from './WebsitePageClient';
 
+// Generate a fixed set of possible IDs (0-99)
+// This means your site will support up to 100 saved websites
+export function generateStaticParams() {
+  return Array.from({ length: 100 }, (_, i) => ({
+    id: i.toString(),
+  }));
+}
+
 export default function WebsitePage() {
   return <WebsitePageClient />;
 } 
